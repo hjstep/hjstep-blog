82d46950d874163484f303c08b8c7b3c655545ac
Heejeong Hwang <heejeongdev@gmail.com>
2024-01-30 21:41:10 +0900

    Chore: rollup 설정

---
+++ rollup.config.js
@@ -0,0 +1,19 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import { terser } from "rollup-plugin-terser";
+import cssbundle from "rollup-plugin-css-bundle";
+
+export default {
+  input: "./index.js", // 진입점 파일 설정
+  output: {
+    file: "dist/index.js", // 번들 파일 경로
+    format: "iife", // 즉시 실행 함수 표현식 (웹 브라우저에서 실행하기 위함)
+    sourcemap: true, // 소스 맵 활성화
+  },
+  plugins: [
+    resolve(),
+    commonjs(),
+    terser(),
+    cssbundle({ output: "./dist/index.css" }),
+  ],
+};
