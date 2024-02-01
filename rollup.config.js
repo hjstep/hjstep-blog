996ec25a928ff60bc06d21e10d9efe67d1cd8f88
Heejeong Hwang <heejeongdev@gmail.com>
2024-02-02 01:07:43 +0900

    Refactor: 불필요한 주석 제거

--- rollup.config.js
+++ rollup.config.js
@@ -5,11 +5,11 @@ import postcss from 'rollup-plugin-postcss';
 import cssnano from 'cssnano';
 
 export default {
-  input: "./index.js", // 진입점 파일 설정
+  input: "./index.js",
   output: {
-    file: "dist/index.js", // 번들 파일 경로
-    format: "iife", // 즉시 실행 함수 표현식 (웹 브라우저에서 실행하기 위함)
-    sourcemap: true, // 소스 맵 활성화
+    file: "dist/index.js",
+    format: "iife",
+    sourcemap: true,
   },
   treeshake: {
     preset: 'recommended'
